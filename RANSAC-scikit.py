1853149c4ccadb3dca947d57c9fa0a67b7a8f648
kazi <k.goni@innosep.de>
2019-12-22 22:18:08 +0000

    ransac implemented in ransac-scikit.py

--- RANSAC-scikit.py
+++ RANSAC-scikit.py
@@ -2,43 +2,42 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn import linear_model, datasets
-
 import math
 
-df = pd.read_csv('scanData.txt',delimiter=',')
-x = df.values[:,0]
-y = df.values[:,1]
+# getting data from a text file
 
 df = pd.read_csv('scanData.txt',delimiter=',')
 angle = df.values[:,0]
 distance = df.values[:,1]
 cartesian = [(r*math.cos(phi*math.pi/180), r*math.sin(phi*math.pi/180)) for r, phi in zip(distance, angle)]
 x, y = map(list, zip(*cartesian))
-print(x)
+#print(x)
 
+
+# coverting this into 2d array
 x=  np.array(x)
 y=  np.array(y)
 
 x=x.reshape(-1, 1)
 y=y.reshape(-1, 1)
-print("x: ",x)
-print("y: ", y)
 
 lr = linear_model.LinearRegression()
 lr.fit(x, y)
-ransac = linear_model.RANSACRegressor(max_trials=1000,min_samples=3)
+ransac = linear_model.RANSACRegressor(max_trials=1000,min_samples=300)
 ransac.fit(x, y)
 
 # Predict data of estimated models
 line_X = np.arange(x.min(), x.max())[:, np.newaxis]
+print(line_X)
 line_y = lr.predict(line_X)
 line_y_ransac = ransac.predict(line_X)
-
+print(line_y_ransac)
 plt.scatter(x,y, color='yellowgreen', marker='.',
             label='Inliers')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=2,
+plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=1,
          label='RANSAC regressor')
 plt.legend(loc='lower right')
 plt.xlabel("Input")
 plt.ylabel("Response")
 plt.show()
+
