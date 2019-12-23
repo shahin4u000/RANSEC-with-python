1b6296194353eb3a0a64c28570bee4f9f6d593ca
Kazi <Kazi.shahinbd@gmail.com>
2019-12-23 11:16:30 +0100

    ransec with sci kit learn implemented

--- test-ransec.py
+++ test-ransec.py
@@ -1,37 +1,43 @@
 import numpy as np
-import pandas as pd
-from sklearn import datasets, linear_model
-from sklearn.linear_model import RANSACRegressor
-from sklearn.ensemble import RandomForestClassifier
 from matplotlib import pyplot as plt
-n_samples = 1000
-n_outliers = 50
-x, y, coef = datasets.make_regression(n_samples=n_samples, n_features=1,
-                                      n_informative=1, noise=10,
-                                      coef=True, random_state=0)
-a = np.arange(15).reshape(3, 5)
-print(a)
-
+import pandas as pd
+from sklearn import linear_model, datasets
 import math
 
-df = pd.read_csv('scanData.txt',delimiter=',')
-x = df.values[:,0]
-y = df.values[:,1]
+# getting data from a text file
+from sklearn.linear_model import RANSACRegressor
 
 df = pd.read_csv('scanData.txt',delimiter=',')
 angle = df.values[:,0]
 distance = df.values[:,1]
-
-
 cartesian = [(r*math.cos(phi*math.pi/180), r*math.sin(phi*math.pi/180)) for r, phi in zip(distance, angle)]
-#print(x,y)
 x, y = map(list, zip(*cartesian))
-print(cartesian)
+#print(x)
+
+
+# coverting this into 2d array
+x=  np.array(x)
+y=  np.array(y)
+
+x=x.reshape(-1, 1)
+y=y.reshape(-1, 1)
+
 lr = linear_model.LinearRegression()
 lr.fit(x, y)
+ransac = RANSACRegressor(max_trials=1000,min_samples=300)
+ransac.fit(x, y)
 
 # Predict data of estimated models
-#line_X = np.arange(x.min(), x.max())[:, np.newaxis]
-#line_y = lr.predict(line_X)
-
-plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
+line_X = np.arange(x.min(), x.max())[:, np.newaxis]
+print(line_X)
+line_y = lr.predict(line_X)
+line_y_ransac = ransac.predict(line_X)
+print(line_y_ransac)
+plt.scatter(x,y, color='yellowgreen', marker='.',
+            label='Inliers')
+plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=1,
+         label='RANSAC regressor')
+plt.legend(loc='lower right')
+plt.xlabel("Input")
+plt.ylabel("Response")
+plt.show()
