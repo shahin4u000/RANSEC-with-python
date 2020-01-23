2d73385819d8beeea244e0763cae462657c235d4
Kazi <Kazi.shahinbd@gmail.com>
2020-01-23 21:13:56 +0100

    dbscan mark combined in file dbscan-by-me.py

--- DBSCAN-by-me.py
+++ DBSCAN-by-me.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from skimage.measure import ransac, LineModelND, CircleModel
 import math
+#import cv2
 
-
-df = pd.read_csv(r'scan-data\capture1.csv',delimiter=',')
+df = pd.read_csv(r'/home/kazi/Desktop/RANSEC-with-python/scan-data/capture3.csv',delimiter=',')
 
 angle = df.values[:,0]
 distance = df.values[:,1]
@@ -54,6 +54,7 @@ print('Estimated number of noise points: %d' % n_noise_)
 
 
 # Black removed and is used for noise instead.
+finalData= np.array([])
 unique_labels = set(labels)
 colors = [plt.cm.Spectral(each)
           for each in np.linspace(0, 1, len(unique_labels))]
@@ -63,14 +64,71 @@ for k, col in zip(unique_labels, colors):
         col = [0, 0, 0, 1]
 
     class_member_mask = (labels == k)
-
+    print('class_member_mask: ', class_member_mask)
     xy = data[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=5)
+    detectedByDBSCAN= np.column_stack([xy[:, 0], xy[:, 1]])
+    
+    if finalData.size == 0:
+        finalData = detectedByDBSCAN
+    elif detectedByDBSCAN.size >=30:
+        finalData = np.concatenate((finalData,detectedByDBSCAN))
+        print('finalData: ', finalData)
+    #update the data with outliers and remove inliers
+    
+   
 
-    xy = data[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=6)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
+    #xy = data[class_member_mask & core_samples_mask]
+plt.plot(finalData[:, 0], finalData[:, 1], 'o', markeredgecolor='k', markersize=5)
 plt.show()
+print("i am size: ",xy)
+
+################
+Thetas = []
+Ranges = []
+maxRange = 0
+
+
+
+for row in finalData:
+    #print("row ",row)
+    r = row[0]
+    theta = row[1]
+    
+    #print(f'DEBUG: theta: {theta}, range: {r}')
+    if r> maxRange:
+        maxRange = r
+        if theta > maxRange:
+            maxRange = theta
+    Ranges.append(r)
+    Thetas.append(theta)
+
+print("maxrange : ",maxRange)
+# Plot what we found - i.e. a triangle centred on lidar stretching out to 2 pts on  walls
+# Work out size of canvs
+maxRange = int(maxRange+1)
+h = w = 2 * maxRange
+cx = cy = maxRange
+# Make blank canvas
+im = np.zeros((h,w), np.uint8)
+# Centre point, in every triangle
+pt1 = (cy, cx)
+# Join up last point with first to complete the circle
+Ranges.append(Ranges[0])
+Thetas.append(Thetas[0])
+for i in range(1,len(Thetas)):
+    r = Ranges[i-1]
+    theta = Thetas[i-1]
+    x = r
+    y = theta
+    pt2 = (cx + x, cy - y)
+    r = Ranges[i]
+    theta = Thetas[i]
+    x = r
+    y = theta
+    pt3 = (cx + x, cy - y)
+    triangleCnt = np.array( [pt1, pt2, pt3] )
+    cv2.drawContours(im, [triangleCnt.astype(int)], 0, 255, -1)
+cv2.imwrite('result.png', im)
+
+#plt.title('Estimated number of clusters: %d' % n_clusters_)
+#plt.show()
