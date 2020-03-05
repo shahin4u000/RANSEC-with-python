923c664b049a8a28abd6edc7e1229b0f761c1265
Kazi <Kazi.shahinbd@gmail.com>
2020-03-05 17:57:41 +0100

    05-03-2020 signed off

--- Untitled-1.py
+++ Untitled-1.py
@@ -749,7 +749,7 @@ ld2.plot_xy()
 
 
 #%%
-##bokeh
+##bokeh https://thedatafrog.com/en/interactive-visualization-bokeh-jupyter/
 output_file("legend.html")
 tools = "pan,wheel_zoom,box_zoom,reset,hover"
 data = {'x_values': ld2.x,
@@ -760,7 +760,51 @@ TOOLTIPS = [('$size', 10)]
 p = figure(tools=tools)
 p.circle(x='x_values', y='y_values',hover_color="red", tooltips="$size : 10", source=source)
 show(p)
+#%%
 
+from bokeh.layouts import grid
+from bokeh.models import Button, TextInput
+from bokeh.plotting import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from docutils.nodes import figure
+import numpy as np
+import pandas as pd
+
+def modify_doc(doc):
+    # same as before
+    source = ColumnDataSource(df)
+    p = figure(tools=tools)
+    p.scatter('x','y', source=source, alpha=0.5)
+    p.add_tools(
+        HoverTool(
+            tooltips=[('value','@value{2.2f}'), 
+                      ('index', '@index')]
+        )
+    )
+    
+    # this function is called when the button is clicked
+    def update():
+        # number of points to be added, taken from input text box
+        n = int(npoints.value)
+        # new sample of points to be added. 
+        # we use the a narrow gaussian centred on (-1, 1), 
+        # and draw the requested number of points
+        sample3 = np.random.multivariate_normal([-1,-1], [[0.05,0],[0,0.05]], n)
+        df_new = pd.DataFrame(sample3, columns=('x','y'))
+        df_new['value'] = np.sqrt(df['x']**2 + df['y']**2)
+        # only the new data is streamed to the bokeh server, 
+        # which is an efficient way to proceed
+        source.stream(df_new)
+    
+    # GUI: 
+    button = Button(label='add points:')
+    npoints = TextInput(value="50")
+    button.on_click(update)
+    # arranging the GUI and the plot. 
+    layout = grid([[button, npoints], p])
+    doc.add_root(layout)
+
+show(modify_doc)
 #%%
 # compute preliminary wall angles
 a = np.array([np.abs(ld2.x), np.abs(ld2.y)])
--- bokeh-plotting.py
+++ bokeh-plotting.py
@@ -1,27 +1,43 @@
-from math import sin
-from random import random
+from bokeh.layouts import grid
+from bokeh.models import Button, TextInput
+from bokeh.plotting import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from docutils.nodes import figure
+import numpy as np
+import pandas as pd
 
-from bokeh.io import output_file, show
-from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper
-from bokeh.palettes import plasma
-from bokeh.plotting import figure
-from bokeh.transform import transform
+def modify_doc(doc):
+    # same as before
+    source = ColumnDataSource(df)
+    p = figure(tools=tools)
+    p.scatter('x','y', source=source, alpha=0.5)
+    p.add_tools(
+        HoverTool(
+            tooltips=[('value','@value{2.2f}'), 
+                      ('index', '@index')]
+        )
+    )
+    
+    # this function is called when the button is clicked
+    def update():
+        # number of points to be added, taken from input text box
+        n = int(npoints.value)
+        # new sample of points to be added. 
+        # we use the a narrow gaussian centred on (-1, 1), 
+        # and draw the requested number of points
+        sample3 = np.random.multivariate_normal([-1,-1], [[0.05,0],[0,0.05]], n)
+        df_new = pd.DataFrame(sample3, columns=('x','y'))
+        df_new['value'] = np.sqrt(df['x']**2 + df['y']**2)
+        # only the new data is streamed to the bokeh server, 
+        # which is an efficient way to proceed
+        source.stream(df_new)
+    
+    # GUI: 
+    button = Button(label='add points:')
+    npoints = TextInput(value="50")
+    button.on_click(update)
+    # arranging the GUI and the plot. 
+    layout = grid([[button, npoints], p])
+    doc.add_root(layout)
 
-list_x = list(range(100))
-list_y = [random() + sin(i / 20) for i in range(100)]
-desc = [str(i) for i in list_y]
-
-source = ColumnDataSource(data=dict(x=list_x, y=list_y, desc=desc))
-hover = HoverTool(tooltips=[
-    ("index", "$index"),
-    ("(x,y)", "(@x, @y)"),
-    ('desc', '@desc'),
-])
-mapper = LinearColorMapper(palette=plasma(256), low=min(list_y), high=max(list_y))
-
-p = figure(plot_width=400, plot_height=400, tools=[hover], title="Belgian test")
-p.circle('x', 'y', size=10, source=source,
-         fill_color=transform('y', mapper))
-
-output_file('test.html')
-show(p)
+show(modify_doc)
