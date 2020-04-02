e5be6021f8803a83698f4fbfddeac21f40c180e0
kazi <k.goni@innosep.de>
2020-04-02 14:38:22 +0200

    rotating is done

--- Untitled-1.py
+++ Untitled-1.py
@@ -759,50 +759,65 @@ output_file("callback.html")
 
 plot = figure(plot_width=400, plot_height=400)
 plot.toolbar.logo = None
-a = []
-b = []
+tapX = []
+tapY = []
 tools = "pan,wheel_zoom,reset,hover"
 data = {'x_values': ld2.x,
         'y_values': ld2.y}
 
 source = ColumnDataSource(data=data)
-source1 = ColumnDataSource(data=dict(x=a, y=b))
+source1 = ColumnDataSource(data=dict(x=tapX, y=tapY))
 lidarPlot= plot.circle(x='x_values', y='y_values',hover_color="red", source=source)
 
 customPlot= plot.scatter('x', 'y', source=source1,
-             line_color='blue', fill_alpha=0.3, size=10)
+             line_color='blue', fill_alpha=0.3, size=5)
 plot.patch('x', 'y',source=source1, line_width=5, color="red", alpha=0.5)
 draw_tool = PointDrawTool(renderers=[customPlot])
 
-callback = CustomJS(args=dict(xy=source1, ab=source), code="""
+callback = CustomJS(args=dict(sensor=source, xy=source1 ), code="""
+                    
+        // getting lidar value and tabX & tapY
         var data = xy.data;
-        var data2 = ab.data;
+        var sensorData = sensor.data;
         
-        var f = cb_obj.value
-        console.log(data, data2)
+        // getting slider value
+        var rotationAngle = cb_obj.value;        
+        var slider = rotationAngle * (Math.PI / 180);
+                    
+        console.log(slider)
+        // 
         var x = data['x'];
         var y = data['y'];
-        var a = data2['x'];
-        var b = data2['y'];
-        
+        var a = sensorData['x_values'];
+        var b = sensorData['y_values'];
+        console.log(a);
+        console.log(a.length);
+        /*
         if(a.includes(cb_obj.x) && b.includes(cb_obj.y)) {
-            x.push(cb_obj.x)
-            y.push(cb_obj.y)
+            x.push(cb_obj.x);
+            y.push(cb_obj.y);
             
         } else {
-            x.push(cb_obj.x)
-            y.push(cb_obj.y)
-            console.log(cb_obj.x)
+            x.push(cb_obj.x);
+            y.push(cb_obj.y);
+            console.log(cb_obj.x);
+        }
+        */
+        
+        for (var i = 0; i < a.length; i++) {
+            a[i] =  a[i]*Math.cos(slider) - b[i]*Math.sin(slider);
+            b[i] =  a[i]*Math.sign(slider) + b[i]*Math.cos(slider); 
         }
         
-        
+        sensor.change.emit();
         xy.change.emit();
     """)
 
-slider = Slider(start=0.1, end=4, value=1, step=.1, title="power")
-slider.js_on_change('value', callback)
+rotation = Slider(start=-180, end=180, value=0, step=0.1, title="Rotate ")
+#callback.args["slider"] = rotation   # sending slider value into the callback function
+rotation.js_on_change('value', callback)
 plot.js_on_event('tap', callback)
-layout = column(slider, plot)
+layout = column(rotation, plot)   # create two row, 1. rotation 2. plotting
 plot.add_tools(draw_tool)
 plot.toolbar.active_tap = draw_tool
 show(layout)
